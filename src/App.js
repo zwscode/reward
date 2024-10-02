a4910949bdfcc7a543f28e355e8e589d237a7e71
Michael Chu <michaelchu1024@gmail.com>
2024-10-02 16:58:16 +0000

    change title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Calculate Reward Points</h1>
       <CalcReward />
     </div>
   );
